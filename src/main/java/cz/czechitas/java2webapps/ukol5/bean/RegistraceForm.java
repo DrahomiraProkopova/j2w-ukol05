d5390d99855e7f7f18e9450cc525d0e35e855d65
Drahomira Prokopova <prokopova.drahomira@gmail.com>
2022-11-06 16:32:11 +0100

    Ukol 5 hotovo

---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/bean/RegistraceForm.java
@@ -0,0 +1,83 @@
+package cz.czechitas.java2webapps.ukol5.bean;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class RegistraceForm {
+
+    @NotBlank
+    private String jmeno;
+
+    @NotBlank
+    private String prijmeni;
+
+    @NotBlank
+    private String datumNarozeni;
+
+    @NotBlank
+    private String pohlavi;
+
+    @NotBlank
+    private String turnus;
+
+    private String telefon;
+
+    @Email
+    private String email;
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public String getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public void setDatumNarozeni(String datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public String getPohlavi() {
+        return pohlavi;
+    }
+
+    public void setPohlavi(String pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public String getTurnus() {
+        return turnus;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,7 +1,21 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
+import cz.czechitas.java2webapps.ukol5.bean.RegistraceForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -9,9 +23,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RegistraceController {
 
-  @GetMapping("/")
-  public String formular() {
-    return "formular";
+  @GetMapping("")
+  public Object formular() {
+    ModelAndView modelAndView = new ModelAndView("/formular");
+    modelAndView.addObject("form", new RegistraceForm());
+    return modelAndView;
+  }
+
+  @PostMapping("/turnus")
+  public Object turnus(@Valid @ModelAttribute("form") RegistraceForm registraceForm, BindingResult bindingResult) {
+
+    if (bindingResult.hasErrors()) {
+      return "/formular";
+    }
+
+    int vek = vypoctiVek(registraceForm);
+    if (vek < 9 || vek > 15) {
+      bindingResult.rejectValue("datumNarozeni", "error.form","věk mezi 9 a 15 roky (včetně)");
+      return "/formular";
+    }
+    return new ModelAndView("/rekapitulace");
+  }
+
+  private static int vypoctiVek(RegistraceForm registraceForm) {
+    String datumNarozeni = registraceForm.getDatumNarozeni();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.u");
+    LocalDate datumNarozeniDate = LocalDate.parse(datumNarozeni, formatter);
+    Period period = datumNarozeniDate.until(LocalDate.now());
+    int vek = period.getYears();
+    return vek;
   }
 
 }
